7c320db044512f84949b02177ab7f969091bfddc
Dasher <dasha.topor@gmail.com>
2024-01-24 18:41:56 +0100

    refactor Co-authored-by: William Malbos <contact@wmalbos.fr>

--- FrontEnd/javascript.js
+++ FrontEnd/javascript.js
@@ -25,5 +25,64 @@ async function displayArchitectWorks() {
     console.log("Architect's Works:", works);
   }
 
-  displayArchitectWorks();
+
+const galleryContainer = document.querySelector('.gallery');
+
+/**
+ * Récupère et affiche la liste des projets
+ */
+newDisplayWorks = () => {
+
+  // On demande au serveur la liste des projets
+    fetch('http://localhost:5678/api/works')
+
+      // On récupère la réponse du serveur 
+      .then(response => {
+        return response.json();  // On ne récupère que le contenu que l'on envoi dans le prochain then
+      })
+
+      // On à la liste des travaux
+      .then(works => {
+
+        // On parcours tous les travaux et à chaque tour de boucle on à la travail en cours dans work
+        works.forEach(work => {
+
+          // Création d'un nouveau noeud "figure"
+          const figure = document.createElement('figure');
+          figure.classList.add('ma-nouvelle-classe') // On peux ajouter une nouvelle classe
+
+          // On modifie le contenu HTML du noeud (attention de ne pas oublier les backtik => ``)
+          figure.innerHTML = `
+            <img src="${work.imageUrl}" alt="${work.title}" />
+            <figcaption>${work.title}</figcaption>
+          `;
+
+          // On ajoute le nouveau noeud dans notre gallery
+          galleryContainer.appendChild(figure);
+
+        });
+      })
+
+      // Déclanche une erreur
+      .catch(error => {
+        console.error('Une erreur est survenue');
+      })
+}
+
+newDisplayWorks();
+
+  //displayArchitectWorks();
+
+
+  /**
+   * TODO : 
+   * - Faire exactement pareil pour les catégories
+   * 
+   * - Faire le design de la page connexion 
+   * - Partie JS: 
+   * --- Récupérer en JS les données du formulaire lorsque l'on clique sur le bouton "connexion"
+   * --- Avec fetch (POST), envoyer ces données pour récupérer un token
+   * 
+   * - Faire la modal en javscript
+   */
   
