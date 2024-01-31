9baff36f8969673b48bb2e7fd056cd33b2de496c
Dasher <dasha.topor@gmail.com>
2024-01-31 12:11:16 +0100

    feat: add styles to category's buttons

--- FrontEnd/javascript.js
+++ FrontEnd/javascript.js
@@ -1,131 +1,118 @@
 async function displayArchitectWorks() {
-    const response = await fetch('http://localhost:5678/api/works');
-    const works = await response.json();
-    console.log("Architect's Works:", works);
+  const response = await fetch("http://localhost:5678/api/works");
+  const works = await response.json();
+  console.log("Architect's Works:", works);
 
-    const galleryContainer = document.querySelector('.gallery');
+  const galleryContainer = document.querySelector(".gallery");
 
-    galleryContainer.innerHTML = '';
-  
-    works.forEach(work => {
-      const figure = document.createElement('figure');
-      const img = document.createElement('img');
-      const figcaption = document.createElement('figcaption');
-  
-      img.src = work.imageUrl;
-      img.alt = work.title;
-      figcaption.textContent = work.title;
-  
-      figure.appendChild(img);
-      figure.appendChild(figcaption);
-  
-      galleryContainer.appendChild(figure);
-    });
-  
-    console.log("Architect's Works:", works);
-  }
+  galleryContainer.innerHTML = "";
 
+  works.forEach((work) => {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
 
-const galleryContainer = document.querySelector('.gallery');
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    figcaption.textContent = work.title;
+
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+
+    galleryContainer.appendChild(figure);
+  });
+
+  console.log("Architect's Works:", works);
+}
+
+const galleryContainer = document.querySelector(".gallery");
 
 /**
  * Récupère et affiche la liste des projets
  */
 newDisplayWorks = () => {
-
   // On demande au serveur la liste des projets
-    fetch('http://localhost:5678/api/works')
+  fetch("http://localhost:5678/api/works")
+    // On récupère la réponse du serveur
+    .then((response) => {
+      return response.json(); // On ne récupère que le contenu que l'on envoi dans le prochain then
+    })
 
-      // On récupère la réponse du serveur 
-      .then(response => {
-        return response.json();  // On ne récupère que le contenu que l'on envoi dans le prochain then
-      })
+    // On à la liste des travaux
+    .then((works) => {
+      // On parcours tous les travaux et à chaque tour de boucle on à la travail en cours dans work
+      works.forEach((work) => {
+        // Création d'un nouveau noeud "figure"
+        const figure = document.createElement("figure");
+        figure.classList.add("ma-nouvelle-classe"); // On peux ajouter une nouvelle classe
 
-      // On à la liste des travaux
-      .then(works => {
-
-        // On parcours tous les travaux et à chaque tour de boucle on à la travail en cours dans work
-        works.forEach(work => {
-
-          // Création d'un nouveau noeud "figure"
-          const figure = document.createElement('figure');
-          figure.classList.add('ma-nouvelle-classe') // On peux ajouter une nouvelle classe
-
-          // On modifie le contenu HTML du noeud (attention de ne pas oublier les backtik => ``)
-          figure.innerHTML = `
+        // On modifie le contenu HTML du noeud (attention de ne pas oublier les backtik => ``)
+        figure.innerHTML = `
             <img src="${work.imageUrl}" alt="${work.title}" />
             <figcaption>${work.title}</figcaption>
           `;
 
-          // On ajoute le nouveau noeud dans notre gallery
-          galleryContainer.appendChild(figure);
+        // On ajoute le nouveau noeud dans notre gallery
+        galleryContainer.appendChild(figure);
+      });
+    })
 
-        });
-      })
-
-      // Déclanche une erreur
-      .catch(error => {
-        console.error('Une erreur est survenue');
-      })
-}
+    // Déclanche une erreur
+    .catch((error) => {
+      console.error("Une erreur est survenue");
+    });
+};
 
 newDisplayWorks();
 
-  //displayArchitectWorks();
+//displayArchitectWorks();
 
+/**
+ * TODO :
+ * - Faire exactement pareil pour les catégories
+ *
+ * - Faire le design de la page connexion
+ * - Partie JS:
+ * --- Récupérer en JS les données du formulaire lorsque l'on clique sur le bouton "connexion"
+ * --- Avec fetch (POST), envoyer ces données pour récupérer un token
+ *
+ * - Faire la modal en javscript
+ */
 
-  /**
-   * TODO : 
-   * - Faire exactement pareil pour les catégories
-   * 
-   * - Faire le design de la page connexion 
-   * - Partie JS: 
-   * --- Récupérer en JS les données du formulaire lorsque l'on clique sur le bouton "connexion"
-   * --- Avec fetch (POST), envoyer ces données pour récupérer un token
-   * 
-   * - Faire la modal en javscript
-   */
-  
-  const categoriesContainer = document.querySelector('.categories');
-  
-  DisplayCategories = () => {
+const categoriesContainer = document.querySelector(".categories");
 
-    fetch('http://localhost:5678/api/categories')
-
-    .then(response => {
-      return response.json();  // On ne récupère que le contenu que l'on envoi dans le prochain then
+DisplayCategories = () => {
+  fetch("http://localhost:5678/api/categories")
+    .then((response) => {
+      return response.json(); // On ne récupère que le contenu que l'on envoi dans le prochain then
     })
 
-    .then(categories => { 
+    .then((categories) => {
+      const allButton = document.createElement("button");
+      allButton.classList.add("category-btn");
+      allButton.setAttribute("id", "all");
 
-      const allButton = document.createElement('button');
-      allButton.classList.add('category-btn');
-      allButton.setAttribute('id', 'all');
-
-      allButton.innerHTML = 'Tous';
+      allButton.innerHTML = "Tous";
 
       categoriesContainer.appendChild(allButton);
 
-    categories.forEach(category => {
+      categories.forEach((category) => {
+        const button = document.createElement("button");
+        button.classList.add("category-btn");
 
-      const button = document.createElement('button');
-      button.classList.add('category-btn');
+        button.setAttribute("id", category.id);
 
-      button.setAttribute('id', category.id);
-
-      button.innerHTML = `
+        button.innerHTML = `
       ${category.name}
       `;
 
-      categoriesContainer.appendChild(button);
-      
+        categoriesContainer.appendChild(button);
       });
-    })  
+    })
 
-    .catch(error => {
-      console.error('Une erreur est survenue');
+    .catch((error) => {
+      console.error("Une erreur est survenue");
     });
-    
-  }
+};
 
-  DisplayCategories();
+DisplayCategories();
